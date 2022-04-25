c65f43b9516587b25d321be23b6e3d4b5f31cd56
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-04-25 16:15:28 -0600

    visualAssets

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ class App extends Component {
                     </div>
 
                     <div className='headerMid'>
-
-
+                        <img src = {busqueda}/>
+                        <h3>Search</h3>
                     </div>
 
                     <div className='headerRight'>
