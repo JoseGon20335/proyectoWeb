511170c7b704d0db92c072833411a4b3d575e663
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-04-28 19:28:31 -0600

    fixBugsAndaddEslint

--- src/App.js
+++ src/App.js
@@ -1,62 +1,58 @@
 import React, { Component } from 'react';
-import './App.css'
+import './App.css';
 import { data_list } from './posts';
 import { emote_list } from './interracion';
 
-import pinterest from './img/logo.png'
-import user from './img/user.png'
-import mensaje from './img/msg.png'
-import busqueda from './img/search.png'
-import notificacion from './img/bell.png'
+import pinterest from './img/logo.png';
+import user from './img/user.png';
+import mensaje from './img/msg.png';
+import busqueda from './img/search.png';
+import notificacion from './img/bell.png';
 
 class App extends Component {
+  render() {
+    return (
+      <div className="background">
+        <div className="header">
+          <div className="headerLeft">
+            <img src={pinterest} />
+            <h3 className="focusBlack">Home</h3>
+            <h3 className="focusWhite">Today</h3>
+          </div>
 
-    render() {
-        return (
-            <div className='background'>
-                <div className='header'>
-                    <div className='headerLeft'>
-                        <img src = {pinterest}/>
-                        <h3 className='focusBlack'>Home</h3>
-                        <h3 className='focusWhite'>Today</h3>
-                    </div>
+          <div className="headerMid">
+            <img src={busqueda} />
+            <h3>Search</h3>
+          </div>
 
-                    <div className='headerMid'>
-                        <img src = {busqueda}/>
-                        <h3>Search</h3>
-                    </div>
+          <div className="headerRight">
+            <img src={notificacion} />
+            <img src={mensaje} />
+            <img src={user} />
+          </div>
+        </div>
 
-                    <div className='headerRight'>
-                        <img src = {notificacion}/>
-                        <img src = {mensaje}/>
-                        <img src = {user}/>
-                    </div>
-                </div>
-
-                <div className='MainContent'>
-                    {
-                        data_list.map((object,index = 0) =>(
-                            <div className='postInfo' key={object.key}>
-                                <img src={object.img}/>
-                                <div className='interracion'>
-                                    {
-                                        emote_list.map((emote,index2=0) =>(
-                                            <div >
-                                                <img src={emote.img}/>
-                                            </div>
-                                        )
-                                        )
+        <div className="MainContent">
+          {
+                        data_list.map((object, index = 0) => (
+                          <div className="postInfo" key={object.key}>
+                            <img src={object.img} />
+                            <div className="interracion">
+                              {
+                                        emote_list.map((emote, index2 = 0) => (
+                                          <div>
+                                            <img src={emote.img} />
+                                          </div>
+                                        ))
                                     }
-                                </div>
                             </div>
-                        )
-                        )
+                          </div>
+                        ))
                     }
-                </div>
-            </div>
-        );
-    }
-
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/interracion.js
+++ src/interracion.js
@@ -1,14 +1,14 @@
 export const emote_list = [
-	{
-		key:"1",
-		img:"https://s.pinimg.com/webapp/wow-3cb697ef.svg",
-	},
-	{
-		key:"2",
-		img:"https://s.pinimg.com/webapp/love-c31e0b8d.svg",
-	},
-	{
-		key:"3",
-		img:"https://s.pinimg.com/webapp/goodIdea-b770896d.svg",
-	},
+  {
+    key: '1',
+    img: 'https://s.pinimg.com/webapp/wow-3cb697ef.svg',
+  },
+  {
+    key: '2',
+    img: 'https://s.pinimg.com/webapp/love-c31e0b8d.svg',
+  },
+  {
+    key: '3',
+    img: 'https://s.pinimg.com/webapp/goodIdea-b770896d.svg',
+  },
 ];
